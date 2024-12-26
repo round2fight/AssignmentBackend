5cd76890eeb87362df7aa2fb9b83a40da3e53271
round2fight <rosdancodes@gmail.com>
2024-12-27 03:44:09 +0530

    Push requirements.txt

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def home():
     return jsonify(message="Hello World")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Ensure it listens on all interfaces
